b8d3f9f8e5b922b4c1ffd5c2c57076188c602ed4
Magnus Grande <magnugra@stud.ntnu.no>
2025-09-18 13:16:39 +0200

    Incremented the number of methods by 1 by implementing the increment method.

--- src/main/java/no/ntnu/idata2302/lab02/Counter.java
+++ src/main/java/no/ntnu/idata2302/lab02/Counter.java
@@ -9,79 +9,85 @@ package no.ntnu.idata2302.lab02;
 
 public class Counter {
 
-    public static void main(String args[]) {
-        var counter = Counter.decimal(5);
-        for (int i = 0; i < 50; i++) {
-            System.out.println(counter.value());
-            counter.increment();
-        }
+  public static void main(String[] args) {
+    var counter = Counter.decimal(5);
+    for (int i = 0; i < 50; i++) {
+      System.out.println(counter.value());
+      counter.increment();
     }
+  }
 
-    /**
-     * Create a new counter for decimal numbers (i.e., base 10)
-     *
-     * @param digitCount the number of digits to include in the counter
-     * @return a new Counter object
-     */
-    public static Counter decimal(int digitCount) {
-        return withAlphabet("0123456789", digitCount);
+  /**
+   * Create a new counter for decimal numbers (i.e., base 10)
+   *
+   * @param digitCount the number of digits to include in the counter
+   * @return a new Counter object
+   */
+  public static Counter decimal(int digitCount) {
+    return withAlphabet("0123456789", digitCount);
+  }
+
+  /**
+   * Create a new counter for binary numbers (i.e., base 2)
+   *
+   * @param digitCount the number of digits in to include in the counter
+   * @return a new counter object
+   */
+  public static Counter binary(int digitCount) {
+    return withAlphabet("01", digitCount);
+  }
+
+  /**
+   * Create a counter of a specific length with a specific alphabet
+   *
+   * @param alphabet   the set of symbols used by the counter
+   * @param digitCount the maximum number of digits
+   */
+  private static Counter withAlphabet(String alphabet, int digitCount) {
+    if (digitCount <= 0)
+      throw new IllegalArgumentException("The number of digits must be positive");
+    assert alphabet != null && !alphabet.isEmpty() : "null or '' are not a valid alphabet";
+
+    var digits = new DigitDisplay[digitCount];
+    for (int i = 0; i < digitCount; i++) {
+      digits[i] = new DigitDisplay(alphabet);
     }
+    return new Counter(digits);
+  }
 
-    /**
-     * Create a new counter for binary numbers (i.e., base 2)
-     *
-     * @param digitCount the number of digits in to include in the counter
-     * @return a new counter object
-     */
-    public static Counter binary(int digitCount) {
-        return withAlphabet("01", digitCount);
+  private final DigitDisplay digits[];
+
+  Counter(DigitDisplay[] digits) {
+    this.digits = digits;
+  }
+
+  /**
+   * Increment the counter by one
+   */
+  public void increment() {
+    boolean carryOver = true;
+    int index = 0;
+
+    while (carryOver && index < digits.length) {
+      digits[index].next();
+      carryOver = digits[index].isZero();
+
+      index++;
     }
+  }
 
-    /**
-     * Create a counter of a specific length with a specific alphabet
-     *
-     * @param alphabet   the set of symbols used by the counter
-     * @param digitCount the maximum number of digits
-     */
-    private static Counter withAlphabet(String alphabet, int digitCount) {
-        if (digitCount <= 0)
-            throw new IllegalArgumentException("The number of digits must be positive");
-        assert alphabet != null && !alphabet.isEmpty()
-            : "null or '' are not a valid alphabet";
-
-        var digits = new DigitDisplay[digitCount];
-        for (int i = 0; i < digitCount; i++) {
-            digits[i] = new DigitDisplay(alphabet);
-        }
-        return new Counter(digits);
+  /**
+   * @return the current value of the counter
+   */
+  public String value() {
+    var buffer = new StringBuilder();
+    for (int i = digits.length - 1; i >= 0; i--) {
+      buffer.append(digits[i].symbol());
     }
-
-    private final DigitDisplay digits[];
-
-    Counter(DigitDisplay[] digits) {
-        this.digits = digits;
-    }
-
-    /**
-     * Increment the counter by one
-     */
-    public void increment() {
-        // TODO: Implement
-        throw new RuntimeException("Not yet implemented.");
-    }
-
-    /**
-     * @return the current value of the counter
-     */
-    public String value() {
-        var buffer = new StringBuilder();
-        for (int i = digits.length - 1; i >= 0; i--) {
-            buffer.append(digits[i].symbol());
-        }
-        return buffer.toString();
-    }
-
+    return buffer.toString();
+  }
 }
+
 
 /**
  * Represent a single-digit display, on a given alphabet
@@ -128,5 +134,4 @@ class DigitDisplay {
     boolean isZero() {
         return index == 0;
     }
-
 }
